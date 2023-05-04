825471deb63956364109160234cd95e231f8ef1d
Maxim-Bolobaiko <maxim.bolobaiko@gmail.com>
2023-05-04 20:38:15 +0300

    admin_panel_added

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,74 @@
 from django.contrib import admin
+from django.contrib.admin import display
 
-# Register your models here.
+from .models import (
+    Favourite,
+    Ingredient,
+    IngredientInRecipe,
+    Recipe,
+    ShoppingCart,
+    Tag,
+)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "color",
+        "slug",
+    )
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "measurement_unit",
+    )
+    list_filter = ("name",)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "author",
+        "recipe_in_favorite",
+    )
+    list_filter = (
+        "author",
+        "name",
+        "tags",
+    )
+
+    def recipe_in_favorite(self, obj):
+        return obj.favorites.count()
+
+    recipe_in_favorite.short_description = "Добавлений в избранное"
+
+
+@admin.register(IngredientInRecipe)
+class IngredientInRecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        "ingredient",
+        "recipe",
+        "amount",
+    )
+
+
+@admin.register(Favourite)
+class FavouriteAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "recipe",
+    )
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "recipe",
+    )
