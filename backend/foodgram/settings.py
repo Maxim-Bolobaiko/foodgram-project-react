65a82277188f38e44cf710d74fe9c15c64c7284f
Maxim-Bolobaiko <maxim.bolobaiko@gmail.com>
2023-05-17 13:55:28 +0300

    db_issue

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -98,12 +98,16 @@ if DEBUG:
 else:
     DATABASES = {
         "default": {
-            "ENGINE": os.getenv("DB_ENGINE"),
-            "NAME": os.getenv("DB_NAME"),
-            "USER": os.getenv("POSTGRES_USER"),
-            "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-            "HOST": os.getenv("DB_HOST"),
-            "PORT": os.getenv("DB_PORT"),
+            "ENGINE": os.getenv(
+                "DB_ENGINE", default="django.db.backends.postgresql"
+            ),
+            "NAME": os.getenv("DB_NAME", default="foodgram"),
+            "USER": os.getenv("POSTGRES_USER", default="foodgram_user"),
+            "PASSWORD": os.getenv(
+                "POSTGRES_PASSWORD", default="123foodgram123"
+            ),
+            "HOST": os.getenv("DB_HOST", default="db"),
+            "PORT": os.getenv("DB_PORT", default="5432"),
         }
     }
 
